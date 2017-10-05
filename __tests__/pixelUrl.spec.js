4974e731d7afb1750b6393ee6ba0df58a70f84e0
Jose Fernandez (magec) <joseferper@gmail.com>
2017-10-05 13:56:16 +0200

    Fix tests

--- __tests__/pixelUrl.spec.js
+++ __tests__/pixelUrl.spec.js
@@ -12,7 +12,7 @@ describe('pixelUrl', () => {
         url: 'https://factorialhr.com/blog?mc=123',
         referrer: 'https://google.com'
       })
-      expect(pixelUrl(dom.window.document)).toEqual('https://factorialhr.com/api/internal/pixel?mc=123&referer=https://google.com/&language=en&landing_page=https://factorialhr.com/blog')
+      expect(pixelUrl(dom.window.document)).toEqual('https://factorialhr.com/internal/pixel?mc=123&referer=https://google.com/&language=en&landing_page=https://factorialhr.com/blog')
     })
   })
 
@@ -22,7 +22,7 @@ describe('pixelUrl', () => {
       dom = new JSDOM(html, {
         url: 'https://staging.factorialhr.es/blog'
       })
-      expect(pixelUrl(dom.window.document)).toEqual('https://factorialhr.com/api/internal/pixel?mc=&referer=&language=es&landing_page=https://staging.factorialhr.es/blog')
+      expect(pixelUrl(dom.window.document)).toEqual('https://staging.factorialhr.com/internal/pixel?mc=&referer=&language=es&landing_page=https://staging.factorialhr.es/blog')
     })
   })
 })
